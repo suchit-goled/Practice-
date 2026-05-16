906739d6eab71e0a8dc070b2883f8e38ce710663
suchit-goled <suchitgoled@gmail.com>
2026-05-16 22:36:17 +0530

    new

---
+++ Reverse.java
@@ -0,0 +1,11 @@
+class Reverse {
+    public static void main(String[] args) {
+
+        String str = "java";
+
+        for(int i = str.length()-1; i >= 0; i--) {
+            System.out.print(str.charAt(i));
+        }
+
+    }
+}
