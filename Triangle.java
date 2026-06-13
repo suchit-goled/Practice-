1ba5d5356815432f87c503032de1c5252b9a6af8
suchit-goled <suchitgoled@gmail.com>
2026-06-13 23:28:43 +0530

    Patterns

---
+++ Triangle.java
@@ -0,0 +1,12 @@
+public class Triangle {
+
+    public static void main(String[] args){
+        int n=6;
+        for(int i=0;i<=n;i++){
+            for(int j=i-1;j<n;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
